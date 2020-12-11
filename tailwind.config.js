0859d2d7d357dc99df2c7c3a3a0d43ebb1755573
Pedro Henrique de Campos Silva <phsilva@informatica.itatiba.sp.gov.br>
2020-12-11 16:21:39 -0300

    Site completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    // overwrite tailwind calsses
+    extend: {
+      // add on the tailwind classes
+      // fontFamily: {
+      //   fun: [ 'Varela Round','sans-serif']
+      // }
+    },
   },
   variants: {
     extend: {},
